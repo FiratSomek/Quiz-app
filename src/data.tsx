54031944eead615af38ee5cf6403fad5888fd704
Anna S. A <anna.atalay@minna.tech>
2024-02-28 10:54:38 +0100

    Add corrections

--- src/data.tsx
+++ src/data.tsx
@@ -1,3 +1,5 @@
+import { OptionClass } from "./components/QuizBoard/QuizBoard";
+
 export interface QuizDataItem {
   type: string;
   difficulty: string;
@@ -9,7 +11,7 @@ export interface QuizDataItem {
 
 export interface QuestionOption {
   title: string;
-  class: string;
+  class?: OptionClass;
 }
 
 export const quizData: QuizDataItem[] = [
@@ -20,10 +22,10 @@ export const quizData: QuizDataItem[] = [
     question: "What is the official name of Prince's backing band?",
     correct_answer: "The Revolution",
     options: [
-      { title: "The Paupers", class: "" },
-      { title: "The Revolution", class: "" },
-      { title: "The Wailers", class: "" },
-      { title: "The Heartbreakers", class: "" },
+      { title: "The Paupers"},
+      { title: "The Revolution"},
+      { title: "The Wailers"},
+      { title: "The Heartbreakers"},
     ],
   },
   {
@@ -34,10 +36,10 @@ export const quizData: QuizDataItem[] = [
       "What was the development code name for the Weatherlight expansion for The Gathering, released in 1997?",
     correct_answer: "Mocha Latte",
     options: [
-      { title: "Decaf", class: "" },
-      { title: "Frappuccino", class: "" },
-      { title: "Mocha Latte", class: "" },
-      { title: "Macchiato", class: "" },
+      { title: "Decaf"},
+      { title: "Frappuccino"},
+      { title: "Mocha Latte"},
+      { title: "Macchiato"},
     ],
   },
   {
@@ -47,10 +49,10 @@ export const quizData: QuizDataItem[] = [
     question: "What national museum will you find in Cooperstown, New York?",
     correct_answer: "National Baseball Hall of Fame",
     options: [
-      { title: "National Baseball Hall of Fame", class: "" },
-      { title: "Metropolitan Museum of Art", class: "" },
-      { title: "National Toy Hall of Fame", class: "" },
-      { title: "Museum of Modern Art", class: "" },
+      { title: "National Baseball Hall of Fame"},
+      { title: "Metropolitan Museum of Art"},
+      { title: "National Toy Hall of Fame"},
+      { title: "Museum of Modern Art"},
     ],
   },
   {
@@ -61,10 +63,10 @@ export const quizData: QuizDataItem[] = [
       "Located in Chile, El Teniente is the world's largest underground mine for what metal?",
     correct_answer: "Copper",
     options: [
-      { title: "Iron", class: "" },
-      { title: "Copper", class: "" },
-      { title: "Nickel", class: "" },
-      { title: "Silver", class: "" },
+      { title: "Iron"},
+      { title: "Copper"},
+      { title: "Nickel"},
+      { title: "Silver"},
     ],
   },
   {
@@ -74,10 +76,10 @@ export const quizData: QuizDataItem[] = [
     question: "How many countries border Kyrgyzstan?",
     correct_answer: "4",
     options: [
-      { title: "3", class: "" },
-      { title: "4", class: "" },
-      { title: "1", class: "" },
-      { title: "6", class: "" },
+      { title: "3"},
+      { title: "4"},
+      { title: "1"},
+      { title: "6"},
     ],
   },
   {
@@ -87,10 +89,10 @@ export const quizData: QuizDataItem[] = [
     question: "The minigun was designed in 1960 by which manufacturer.",
     correct_answer: "General Electric",
     options: [
-      { title: "General Electric", class: "" },
-      { title: "Colt Firearms", class: "" },
-      { title: "Heckler Koch", class: "" },
-      { title: "Sig Sauer", class: "" },
+      { title: "General Electric"},
+      { title: "Colt Firearms"},
+      { title: "Heckler Koch"},
+      { title: "Sig Sauer"},
     ],
   },
   {
@@ -100,10 +102,10 @@ export const quizData: QuizDataItem[] = [
     question: "What is the name of Batman's parents?",
     correct_answer: "Thomas & Martha",
     options: [
-      { title: "Joey & Jackie", class: "" },
-      { title: "Jason & Sarah", class: "" },
-      { title: "Todd & Mira", class: "" },
-      { title: "Thomas & Martha", class: "" },
+      { title: "Joey & Jackie"},
+      { title: "Jason & Sarah"},
+      { title: "Todd & Mira"},
+      { title: "Thomas & Martha"},
     ],
   },
   {
@@ -113,10 +115,10 @@ export const quizData: QuizDataItem[] = [
     question: "What are the four corner states of the US?",
     correct_answer: "Utah, Colorado, Arizona, New Mexico",
     options: [
-      { title: "Oregon, Idaho, Nevada, Utah", class: "" },
-      { title: "Kansas, Oklahoma, Arkansas, Louisiana", class: "" },
-      { title: "South Dakota, Minnesota, Nebraska, Iowa", class: "" },
-      { title: "Utah, Colorado, Arizona, New Mexico", class: "" },
+      { title: "Oregon, Idaho, Nevada, Utah"},
+      { title: "Kansas, Oklahoma, Arkansas, Louisiana"},
+      { title: "South Dakota, Minnesota, Nebraska, Iowa"},
+      { title: "Utah, Colorado, Arizona, New Mexico"},
     ],
   },
   {
@@ -126,10 +128,10 @@ export const quizData: QuizDataItem[] = [
     question: "When did the Crisis of the Third Century begin?",
     correct_answer: "235 AD",
     options: [
-      { title: "235 BC", class: "" },
-      { title: "242 AD", class: "" },
-      { title: "235 AD", class: "" },
-      { title: "210 AD", class: "" },
+      { title: "235 BC"},
+      { title: "242 AD"},
+      { title: "235 AD"},
+      { title: "210 AD"},
     ],
   },
   {
@@ -139,10 +141,10 @@ export const quizData: QuizDataItem[] = [
     question: "Which dictator killed the most people?",
     correct_answer: "Mao Zedong",
     options: [
-      { title: "Mao Zedong", class: "" },
-      { title: "Joseph Stalin", class: "" },
-      { title: "Adolf Hitler", class: "" },
-      { title: "Kim Il Sung", class: "" },
+      { title: "Mao Zedong"},
+      { title: "Joseph Stalin"},
+      { title: "Adolf Hitler"},
+      { title: "Kim Il Sung"},
     ],
   },
   {
@@ -152,10 +154,10 @@ export const quizData: QuizDataItem[] = [
     question: "Who is the lead singer of Arctic Monkeys?",
     correct_answer: "Alex Turner",
     options: [
-      { title: "Jamie Cook", class: "" },
-      { title: "Alex Turner", class: "" },
-      { title: "Matt Helders", class: "" },
-      { title: "Nick Malley", class: "" },
+      { title: "Jamie Cook"},
+      { title: "Alex Turner"},
+      { title: "Matt Helders"},
+      { title: "Nick Malley"},
     ],
   },
   {
@@ -165,10 +167,10 @@ export const quizData: QuizDataItem[] = [
     question: "What year was the game Team Fortress 2 released?",
     correct_answer: "2007",
     options: [
-      { title: "2009", class: "" },
-      { title: "2005", class: "" },
-      { title: "2010", class: "" },
-      { title: "2007", class: "" },
+      { title: "2009"},
+      { title: "2005"},
+      { title: "2010"},
+      { title: "2007"},
     ],
   },
   {
@@ -178,10 +180,10 @@ export const quizData: QuizDataItem[] = [
     question: "Which English guitarist has the nickname Slowhand?",
     correct_answer: "Eric Clapton",
     options: [
-      { title: "Mark Knopfler", class: "" },
-      { title: "Eric Clapton", class: "" },
-      { title: "Jeff Beck", class: "" },
-      { title: "Jimmy Page", class: "" },
+      { title: "Mark Knopfler"},
+      { title: "Eric Clapton"},
+      { title: "Jeff Beck"},
+      { title: "Jimmy Page"},
     ],
   },
   {
@@ -191,10 +193,10 @@ export const quizData: QuizDataItem[] = [
     question: "In South Park, what is Stan's surname?",
     correct_answer: "Marsh",
     options: [
-      { title: "Marsh", class: "" },
-      { title: "Stotch", class: "" },
-      { title: "Broflovski", class: "" },
-      { title: "Tweak", class: "" },
+      { title: "Marsh"},
+      { title: "Stotch"},
+      { title: "Broflovski"},
+      { title: "Tweak"},
     ],
   },
   {
@@ -204,10 +206,10 @@ export const quizData: QuizDataItem[] = [
     question: "Which team won 2014 FIFA World Cup in Brazil?",
     correct_answer: "Germany",
     options: [
-      { title: "Argentina", class: "" },
-      { title: "Brazil", class: "" },
-      { title: "Germany", class: "" },
-      { title: "Netherlands", class: "" },
+      { title: "Argentina"},
+      { title: "Brazil"},
+      { title: "Germany"},
+      { title: "Netherlands"},
     ],
   },
 ];
